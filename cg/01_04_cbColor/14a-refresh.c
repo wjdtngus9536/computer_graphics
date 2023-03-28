e8390da6672a4ba8c335ffe4caeeb8e15f287f03
wjdtngus9536 <wjdtngus9536@gmail.com>
2023-03-28 18:08:54 +0900

    update

---
+++ cg/01_04_cbColor/14a-refresh.c
@@ -0,0 +1,53 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma warning(disable: 4711 4710 4100)
+#include <stdio.h>
+#include <string.h> // for strrchr()
+
+const unsigned int WIN_W = 300; // window size in pixels, (Width, Height)
+const unsigned int WIN_H = 300;
+const unsigned int WIN_X = 100; // window position in pixels, (X, Y) 
+const unsigned int WIN_Y = 100;
+
+void refreshFunc(GLFWwindow* window) {
+	// refresh
+	printf("refresh called\n");
+	fflush(stdout);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFinish();
+	// GLFW action
+	glfwSwapBuffers(window);
+}
+
+int main(int argc, char* argv[]) {
+	// get your program name
+#if defined(_WIN32) || defined(_WIN64)
+	char* win_name = (strrchr(argv[0], '\\') == NULL) ? argv[0] : (strrchr(argv[0], '\\') + 1);
+#else // Unix, Linux, MacOS
+	char* win_name = (strrchr(argv[0], '/') == NULL) ? argv[0] : (strrchr(argv[0], '/') + 1);
+#endif
+	// start GLFW & GLEW
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, win_name, NULL, NULL);
+	glfwSetWindowPos(window, WIN_X, WIN_Y);
+	glfwMakeContextCurrent(window);
+	glewInit();
+	// prepare
+	glfwSetWindowRefreshCallback(window, refreshFunc);
+	glClearColor(0.5F, 0.8F, 0.8F, 1.0F);
+	// main loop
+	while (! glfwWindowShouldClose(window)) {
+		// draw
+		glClear(GL_COLOR_BUFFER_BIT);
+		glFinish();
+		// GLFW actions
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	// done
+	glfwTerminate();
+	return 0;
+}
